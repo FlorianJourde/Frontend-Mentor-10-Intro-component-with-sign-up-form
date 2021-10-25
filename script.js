4902b1f94f5d9dfc9d8aa0e8a7a86d497db26df0
Yazdun <yazduntube@gmail.comm>
2021-10-25 16:34:22 +0330

    commit

--- script.js
+++ script.js
@@ -1,79 +1,81 @@
 const form = document.querySelector("form");
 const liveRegion = document.querySelector(".input-aria-live");
 
-const testEmail = email=>{
+const testEmail = (email) => {
   let re = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
   return re.test(email);
-}
+};
 
-const setErrors = ( input , errorId ) =>{
+// set error
+
+const setErrors = (input, errorId) => {
   input.setAttribute("aria-invalid", "true");
   input.setAttribute("aria-describedBy", errorId);
   input.classList.add("hasError");
   input.classList.add("input-error");
   input.classList.add("show-error");
-  input.nextElementSibling.style.maxHeight = input.nextElementSibling.scrollHeight + "px";
+  input.nextElementSibling.style.maxHeight =
+    input.nextElementSibling.scrollHeight + "px";
   input.nextElementSibling.style.marginBottom = 1 + "rem";
-}
+};
 
-const removeErrors = input =>{
-  input.removeAttribute("aria-invalid");
-  input.removeAttribute("aria-describedBy");
-  input.classList.remove("hasError");
-  input.classList.remove("input-error");
-  input.classList.remove("show-error");
+// remove error
+
+const removeErrorHandler = (input, classes, attribures) => {
+  classes.forEach((element) => {
+    input.classList.remove(element);
+  });
+  attribures.forEach((element) => {
+    input.removeAttribute(element);
+  });
+};
+
+const removeErrors = (input) => {
+  removeErrorHandler(
+    input,
+    ["hasError", "input-error", "show-error"],
+    ["aria-invalid", "aria-describedBy"]
+  );
   input.nextElementSibling.style.maxHeight = null;
   input.nextElementSibling.style.marginBottom = 0 + "rem";
-}
+};
 
-const testInputs = ( input, errorId ) =>{
-
-  if ( input.name === "email") {
-    if ( !testEmail(input.value) ) return true;
+const testInputs = (input) => {
+  if (input.name === "email") {
+    if (!testEmail(input.value)) return true;
   }
-  if ( !input.value ) return true;
+  if (!input.value) return true;
 
   return false;
-}
+};
 
-const handleFormSubmit = event =>{
+// handle form submit
+
+const handleErrors = (element, errorType, errorPush, errorPushArr) => {
+  if (testInputs(element)) {
+    setErrors(element, errorType);
+    errorPushArr.push(errorPush);
+  } else {
+    removeErrors(element);
+  }
+};
+
+const handleFormSubmit = (event) => {
   event.preventDefault();
   const errorMessage = [];
   const { firstName, lastName, email, password } = event.target;
 
-  if ( testInputs(firstName) ) {
-    setErrors(firstName, "errorFirstName");
-    errorMessage.push("firstname");
-  } else {
-    removeErrors(firstName);
-  }
+  handleErrors(firstName, "errorFirstName", "firstname", errorMessage);
+  handleErrors(lastName, "errorLastName", "lastname", errorMessage);
+  handleErrors(email, "errorEmail", "email", errorMessage);
+  handleErrors(password, "errorPassword", "password", errorMessage);
 
-  if ( testInputs(lastName) ) {
-    setErrors(lastName, "errorLastName");
-    errorMessage.push("lastname");
-  } else {
-    removeErrors(lastName);
-  }
-
-  if ( testInputs(email) ) {
-    setErrors(email, "errorEmail");
-    errorMessage.push("email");
-  } else {
-    removeErrors(email);
-  }
-
-  if ( testInputs(password) ) {
-    setErrors(password, "errorPassword");
-    errorMessage.push("password");
-  } else {
-    removeErrors(password);
-  }
-
-  if ( errorMessage.length > 0 ) {
-    liveRegion.textContent = "Form submission invalid. Check your " + errorMessage.join(", ") + " input fields";
-  } else {
-    liveRegion.textContent = "Form submission success !"
-  }
-}
+  errorMessage.length > 0
+    ? (liveRegion.textContent =
+        "Form submission invalid. Check your " +
+        errorMessage.join(", ") +
+        " input fields")
+    : (liveRegion.textContent = "Form submission success !");
+};
 
 form.addEventListener("submit", handleFormSubmit);
